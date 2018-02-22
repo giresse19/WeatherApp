c695d4d5075efe466c427fbf75e39adea0491847
Giresse Ashu-Bob <ashubobg@yahoo.co.uk>
2018-02-22 02:37:03 +0200

    labeling graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import  {connect} from "react-redux";
+import  {connect} from 'react-redux';
 import Chart from '../components/chart';
 
  class WeatherList extends Component {
@@ -8,13 +8,13 @@ import Chart from '../components/chart';
          const temps = cityData.list.map(weather => weather.main.temp);
          const pressures = cityData.list.map(weather => weather.main.pressure);
          const humidities = cityData.list.map(weather => weather.main.humidity);
-         console.log(temps);
+         // console.log(temps);
          return (
              <tr key = {name}>
                  <td>{name}</td>
-                 <td><Chart data={temps} color="orange" /></td>
-                 <td><Chart data={pressures} color="blue" /></td>
-                 <td><Chart data={humidities} color="red" /></td>
+                 <td><Chart data={temps} color="orange" units="k"/></td>
+                 <td><Chart data={pressures} color="blue" units="hPa"/></td>
+                 <td><Chart data={humidities} color="red" units="%"/></td>
              </tr>
          );
      }
@@ -24,9 +24,9 @@ import Chart from '../components/chart';
                 <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature (k)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
                 </thead>
                 <tbody>
